86785ba55b69a4442470667fefe93cef6476213e
wangminxiang <ryougi.nevermore@hotmail.com>
2023-02-21 05:26:25 +0800

    add spi

---
+++ raft.go
@@ -0,0 +1,26 @@
+package raft
+
+import (
+	"context"
+	"time"
+)
+
+func New() (r Raft, err error) {
+
+	return
+}
+
+type Raft interface {
+	// Run start node
+	//
+	// check cluster was serving, when serving, then call leader to add this node,
+	// when not serving, then boot a cluster.
+	Run(ctx context.Context) (err error)
+	// Close shutdown node
+	//
+	// check leader, when this node is leader, then just shutdown,
+	// when this nod is not leader, then call leader to remove this node and shutdown.
+	Close(ctx context.Context) (err error)
+	// Apply make fsm to apply a log
+	Apply(ctx context.Context, key []byte, body []byte, timeout time.Duration) (result []byte, err error)
+}
