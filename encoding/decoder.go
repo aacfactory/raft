790eafbc1da9e8d96562b9ef2eef867078970c74
wangminxiang <ryougi.nevermore@hotmail.com>
2023-02-21 10:54:42 +0800

    add encoder

---
+++ encoding/decoder.go
@@ -0,0 +1,106 @@
+package encoding
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"github.com/valyala/bytebufferpool"
+	"io"
+	"time"
+)
+
+func NewDecoder(p []byte) *Decoder {
+	return &Decoder{
+		reader: bytes.NewReader(p),
+	}
+}
+
+type Decoder struct {
+	reader *bytes.Reader
+}
+
+func (decoder *Decoder) Uint64() (v uint64, err error) {
+	p := make([]byte, 8)
+	n, readErr := decoder.reader.Read(p)
+	if readErr != nil {
+		err = fmt.Errorf("decoder get uint64 failed, %v", readErr)
+		return
+	}
+	if n != 8 {
+		err = fmt.Errorf("decoder get uint64 failed, invalid content")
+		return
+	}
+	v = binary.BigEndian.Uint64(p)
+	return
+}
+
+func (decoder *Decoder) Bool() (v bool, err error) {
+	p, readErr := decoder.Uint64()
+	if readErr != nil {
+		err = fmt.Errorf("decoder get bool failed, %v", readErr)
+		return
+	}
+	v = p > 0
+	return
+}
+
+func (decoder *Decoder) Time() (v time.Time, err error) {
+	p, readErr := decoder.Uint64()
+	if readErr != nil {
+		err = fmt.Errorf("decoder get time failed, %v", readErr)
+		return
+	}
+	v = time.Unix(0, int64(p))
+	return
+}
+
+func (decoder *Decoder) Raw() (p []byte, err error) {
+	buf := bytebufferpool.Get()
+	for {
+		b := make([]byte, 1024)
+		n, readErr := decoder.reader.Read(b)
+		if readErr != nil {
+			if readErr == io.EOF {
+				break
+			}
+			bytebufferpool.Put(buf)
+			err = fmt.Errorf("decoder get raw failed, %v", readErr)
+			return
+		}
+		if n > 0 {
+			_, _ = buf.Write(b[0:n])
+		}
+	}
+	p = buf.Bytes()
+	bytebufferpool.Put(buf)
+	return
+}
+
+func (decoder *Decoder) LengthFieldBasedFrame() (p []byte, err error) {
+	length, lengthErr := decoder.Uint64()
+	if lengthErr != nil {
+		err = fmt.Errorf("decoder get length field frame failed, %v", lengthErr)
+		return
+	}
+	if length == 0 {
+		p = make([]byte, 0, 1)
+		return
+	}
+	p = make([]byte, length)
+	n, readErr := decoder.reader.Read(p)
+	if readErr != nil {
+		p = nil
+		err = fmt.Errorf("decoder get length field frame failed, %v", readErr)
+		return
+	}
+	if uint64(n) != length {
+		p = nil
+		err = fmt.Errorf("decoder get length field frame failed, invalid length")
+		return
+	}
+	return
+}
+
+func (decoder *Decoder) Close() {
+	decoder.reader.Reset([]byte{})
+}
---
+++ encoding/encoder.go
@@ -0,0 +1,65 @@
+package encoding
+
+import (
+	"encoding/binary"
+	"github.com/valyala/bytebufferpool"
+	"time"
+)
+
+func NewEncoder() *Encoder {
+	return &Encoder{
+		buf: bytebufferpool.Get(),
+	}
+}
+
+type Encoder struct {
+	buf *bytebufferpool.ByteBuffer
+}
+
+func (encoder *Encoder) Bytes() (p []byte) {
+	p = encoder.buf.Bytes()
+	return
+}
+
+func (encoder *Encoder) WriteUint64(v uint64) {
+	p := make([]byte, 8)
+	binary.BigEndian.PutUint64(p, v)
+	_, _ = encoder.buf.Write(p)
+	return
+}
+
+func (encoder *Encoder) WriteBool(v bool) {
+	n := uint64(0)
+	if v {
+		n = uint64(1)
+	}
+	encoder.WriteUint64(n)
+	return
+}
+
+func (encoder *Encoder) WriteTime(v time.Time) {
+	encoder.WriteUint64(uint64(v.UnixNano()))
+	return
+}
+
+func (encoder *Encoder) WriteRaw(p []byte) {
+	if p == nil || len(p) == 0 {
+		return
+	}
+	_, _ = encoder.buf.Write(p)
+}
+
+func (encoder *Encoder) WriteLengthFieldBasedFrame(p []byte) {
+	if p == nil {
+		p = []byte{}
+	}
+	length := uint64(len(p))
+	encoder.WriteUint64(length)
+	if length > 0 {
+		encoder.WriteRaw(p)
+	}
+}
+
+func (encoder *Encoder) Close() {
+	bytebufferpool.Put(encoder.buf)
+}
