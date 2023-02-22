8dd6d7257bffd0b6c5b4eafdfabd1294250b90ef
wangminxiang <ryougi.nevermore@hotmail.com>
2023-02-22 09:19:53 +0800

    mod meta

--- meta.go
+++ meta.go
@@ -4,3 +4,23 @@ type MetaStore interface {
 	Set(key []byte, val []byte) (err error)
 	Get(key []byte) (val []byte, err error)
 }
+
+func FileMetaStore(path string) (meta MetaStore) {
+	// todo
+	meta = &fileMetaStore{}
+	panic("todo")
+	return
+}
+
+type fileMetaStore struct {
+}
+
+func (meta *fileMetaStore) Set(key []byte, val []byte) (err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (meta *fileMetaStore) Get(key []byte) (val []byte, err error) {
+	//TODO implement me
+	panic("implement me")
+}
