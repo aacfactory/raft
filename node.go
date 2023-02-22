b4b0631d0f241c379eb3c6bd39eae83440533965
wangminxiang <ryougi.nevermore@hotmail.com>
2023-02-22 09:20:08 +0800

    mod node

--- node.go
+++ node.go
@@ -1,5 +1,25 @@
 package raft
 
+import (
+	"context"
+	"time"
+)
+
 type Node struct {
 	aides
+}
+
+func (node *Node) Run(ctx context.Context) (err error) {
+
+	return
+}
+
+func (node *Node) Close(ctx context.Context) (err error) {
+
+	return
+}
+
+func (node *Node) Apply(ctx context.Context, key []byte, body []byte, timeout time.Duration) (result []byte, err error) {
+
+	return
 }
