274030007547799bc0fbac77eb5dfff45e951f15
wangminxiang <ryougi.nevermore@hotmail.com>
2023-02-25 08:23:30 +0800

    mod options

--- options.go
+++ options.go
@@ -7,18 +7,100 @@ import (
 	"time"
 )
 
-func DefaultOptions(id string, fsm FSM, store Store, snapshot SnapshotStore, addresses Addresses, transport Transport) Options {
-	return Options{
-		Id:        id,
-		Addresses: addresses,
-		Nonvoter:  false,
-		TLS:       nil,
-		Leader:    DefaultLeaderOptions(),
-		Snapshot:  DefaultSnapshotOptions(snapshot),
-		Store:     store,
-		FSM:       fsm,
-		Transport: transport,
+func NewOptionsBuilder() (builder *OptionsBuilder) {
+	builder = &OptionsBuilder{
+		options: &Options{
+			Id:        "",
+			Addresses: nil,
+			Nonvoter:  false,
+			TLS:       nil,
+			Leader: LeaderOptions{
+				HeartbeatTimeout: 1000 * time.Millisecond,
+				ElectionTimeout:  1000 * time.Millisecond,
+				CommitTimeout:    50 * time.Millisecond,
+				LeaseTimeout:     500 * time.Millisecond,
+			},
+			Snapshot: SnapshotOptions{
+				Store:                    nil,
+				TrailingLogs:             10240,
+				Interval:                 120 * time.Second,
+				Threshold:                8192,
+				NoSnapshotRestoreOnStart: false,
+			},
+			Store:     nil,
+			FSM:       nil,
+			Transport: nil,
+		},
 	}
+	return
+}
+
+type OptionsBuilder struct {
+	options *Options
+}
+
+func (builder *OptionsBuilder) Id(id string) {
+	builder.options.Id = id
+}
+
+func (builder *OptionsBuilder) Fsm(fsm FSM) {
+	builder.options.FSM = fsm
+}
+
+func (builder *OptionsBuilder) Store(store Store) {
+	builder.options.Store = store
+}
+
+func (builder *OptionsBuilder) SnapshotStore(store SnapshotStore) {
+	builder.options.Snapshot.Store = store
+}
+
+func (builder *OptionsBuilder) SnapshotTrailingLogs(trailingLogs uint64) {
+	builder.options.Snapshot.TrailingLogs = trailingLogs
+}
+
+func (builder *OptionsBuilder) SnapshotThreshold(threshold uint64) {
+	builder.options.Snapshot.Threshold = threshold
+}
+
+func (builder *OptionsBuilder) SnapshotInterval(interval time.Duration) {
+	builder.options.Snapshot.Interval = interval
+}
+
+func (builder *OptionsBuilder) SnapshotNotRestoreOnStart(yes bool) {
+	builder.options.Snapshot.NoSnapshotRestoreOnStart = yes
+}
+
+func (builder *OptionsBuilder) Addresses(addresses Addresses) {
+	builder.options.Addresses = addresses
+}
+
+func (builder *OptionsBuilder) Transport(transport Transport) {
+	builder.options.Transport = transport
+}
+
+func (builder *OptionsBuilder) Nonvoter(yes bool) {
+	builder.options.Nonvoter = yes
+}
+
+func (builder *OptionsBuilder) TLS(v TLS) {
+	builder.options.TLS = v
+}
+
+func (builder *OptionsBuilder) LeaderHeartbeatTimeout(timeout time.Duration) {
+	builder.options.Leader.HeartbeatTimeout = timeout
+}
+
+func (builder *OptionsBuilder) LeaderElectionTimeout(timeout time.Duration) {
+	builder.options.Leader.ElectionTimeout = timeout
+}
+
+func (builder *OptionsBuilder) LeaderCommitTimeout(timeout time.Duration) {
+	builder.options.Leader.CommitTimeout = timeout
+}
+
+func (builder *OptionsBuilder) LeaderLeaseTimeout(timeout time.Duration) {
+	builder.options.Leader.LeaseTimeout = timeout
 }
 
 type Options struct {
@@ -61,25 +143,16 @@ func (options Options) Verify() (err error) {
 	return
 }
 
-func DefaultLeaderOptions() LeaderOptions {
-	return LeaderOptions{
-		HeartbeatTimeout:   1000 * time.Millisecond,
-		ElectionTimeout:    1000 * time.Millisecond,
-		CommitTimeout:      50 * time.Millisecond,
-		LeaderLeaseTimeout: 500 * time.Millisecond,
-	}
-}
-
 type LeaderOptions struct {
 	HeartbeatTimeout time.Duration
 	ElectionTimeout  time.Duration
 	CommitTimeout    time.Duration
-	// LeaderLeaseTimeout
+	// LeaseTimeout
 	// 用于控制“租约”的持续时间
 	// 作为领导者而无法联系法定人数
 	// 个节点。如果我们在没有联系的情况下达到这个间隔，我们将
 	// 辞去领导职务。
-	LeaderLeaseTimeout time.Duration
+	LeaseTimeout time.Duration
 }
 
 func (options LeaderOptions) Verify() (err error) {
@@ -93,17 +166,42 @@ func (options LeaderOptions) Verify() (err error) {
 	if options.CommitTimeout < 1*time.Millisecond {
 		errs.Append(errors.ServiceError("commit timeout is too low"))
 	}
-	if options.LeaderLeaseTimeout < 5*time.Millisecond {
+	if options.LeaseTimeout < 5*time.Millisecond {
 		errs.Append(errors.ServiceError("leader lease timeout is too low"))
 	}
-	if options.LeaderLeaseTimeout > options.HeartbeatTimeout {
-		errs.Append(errors.ServiceError(fmt.Sprintf("leader lease timeout (%s) cannot be larger than heartbeat timeout (%s)", options.LeaderLeaseTimeout, options.HeartbeatTimeout)))
+	if options.LeaseTimeout > options.HeartbeatTimeout {
+		errs.Append(errors.ServiceError(fmt.Sprintf("leader lease timeout (%s) cannot be larger than heartbeat timeout (%s)", options.LeaseTimeout, options.HeartbeatTimeout)))
 	}
 	if options.ElectionTimeout < options.HeartbeatTimeout {
 		errs.Append(errors.ServiceError(fmt.Sprintf("election timeout (%s) must be equal or greater than heartbeat Timeout (%s)", options.ElectionTimeout, options.HeartbeatTimeout)))
 	}
 	if len(errs) > 0 {
 		err = errors.ServiceError("verify leader options failed").WithCause(errs.Error())
+	}
+	return
+}
+
+type SnapshotOptions struct {
+	Store SnapshotStore
+	// TrailingLogs控制快照后留下的日志数量。这是为了让我们可以快速回放跟踪者的日志，而不是被迫发送整个快照。此处传递的值是使用的初始设置。这可以在运行期间使用ReloadConfig进行调整。
+	TrailingLogs uint64
+	// SnapshotThreshold控制在执行快照之前必须有多少未完成的日志。这是为了通过重放一小组日志来防止过度快照。此处传递的值是使用的初始设置。这可以在运行期间使用ReloadConfig进行调整。
+	Threshold                uint64
+	Interval                 time.Duration
+	NoSnapshotRestoreOnStart bool
+}
+
+func (options SnapshotOptions) Verify() (err error) {
+	errs := errors.MakeErrors()
+	if options.Store == nil {
+		errs.Append(errors.ServiceError("snapshot store is too low"))
+	}
+	if options.Interval < 5*time.Millisecond {
+		errs.Append(errors.ServiceError("snapshot interval is too low"))
+	}
+	err = errs.Error()
+	if err != nil {
+		err = errors.ServiceError("verify snapshot options failed").WithCause(err)
 	}
 	return
 }
--- snapshot.go
+++ snapshot.go
@@ -17,42 +17,6 @@ import (
 	"time"
 )
 
-func DefaultSnapshotOptions(store SnapshotStore) SnapshotOptions {
-	return SnapshotOptions{
-		Store:                    store,
-		TrailingLogs:             10240,
-		SnapshotInterval:         120 * time.Second,
-		SnapshotThreshold:        8192,
-		NoSnapshotRestoreOnStart: false,
-	}
-}
-
-type SnapshotOptions struct {
-	Store SnapshotStore
-	// TrailingLogs控制快照后留下的日志数量。这是为了让我们可以快速回放跟踪者的日志，而不是被迫发送整个快照。此处传递的值是使用的初始设置。这可以在运行期间使用ReloadConfig进行调整。
-	TrailingLogs uint64
-	// SnapshotThreshold控制在执行快照之前必须有多少未完成的日志。这是为了通过重放一小组日志来防止过度快照。此处传递的值是使用的初始设置。这可以在运行期间使用ReloadConfig进行调整。
-	SnapshotThreshold uint64
-	SnapshotInterval  time.Duration
-
-	NoSnapshotRestoreOnStart bool
-}
-
-func (options SnapshotOptions) Verify() (err error) {
-	errs := errors.MakeErrors()
-	if options.Store == nil {
-		errs.Append(errors.ServiceError("snapshot store is too low"))
-	}
-	if options.SnapshotInterval < 5*time.Millisecond {
-		errs.Append(errors.ServiceError("snapshot interval is too low"))
-	}
-	err = errs.Error()
-	if err != nil {
-		err = errors.ServiceError("verify snapshot options failed").WithCause(err)
-	}
-	return
-}
-
 type SnapshotSink interface {
 	io.WriteCloser
 	Id() string
