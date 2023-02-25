3017f7bbddbf301bceefe15f0edf2e7b1204810b
wangminxiang <ryougi.nevermore@hotmail.com>
2023-02-25 08:02:32 +0800

    mod log

--- log.go
+++ log.go
@@ -3,7 +3,6 @@ package raft
 import (
 	"fmt"
 	"github.com/aacfactory/raft/encoding"
-	"github.com/aacfactory/rings"
 	"time"
 )
 
@@ -35,6 +34,7 @@ type Log struct {
 	Data       []byte
 	Extensions []byte
 	AppendedAt time.Time
+	Committed  bool
 }
 
 func (log *Log) encodeTo(encoder *encoding.Encoder) (p []byte) {
@@ -79,82 +79,5 @@ func (log *Log) decodeFrom(decoder *encoding.Decoder) (err error) {
 		err = fmt.Errorf("log decode failed, %v", err)
 		return
 	}
-	return
-}
-
-type LogStore interface {
-	FirstIndex(key []byte) (uint64, error)
-	LastIndex(key []byte) (uint64, error)
-	GetLog(index uint64, key []byte) (log *Log, err error)
-	StoreLog(log *Log) error
-	StoreLogs(logs []*Log) error
-	DeleteRange(min, max uint64) error
-}
-
-func FileLogStore(path string) (store LogStore) {
-	// todo
-	store = &fileLogStore{}
-	panic("todo")
-	return
-}
-
-type fileLogStore struct {
-}
-
-func (store *fileLogStore) FirstIndex(key []byte) (uint64, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (store *fileLogStore) LastIndex(key []byte) (uint64, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (store *fileLogStore) GetLog(index uint64, key []byte) (log *Log, err error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (store *fileLogStore) StoreLog(log *Log) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (store *fileLogStore) StoreLogs(logs []*Log) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (store *fileLogStore) DeleteRange(min, max uint64) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-type LogHandlers struct {
-	handlers *rings.HashRing[*LogHandler]
-}
-
-func (handlers *LogHandlers) Dispatch(request *AppendEntriesRequest) (response *AppendEntriesResponse) {
-	handler, got := handlers.handlers.Get(request.Key)
-	if !got {
-		// todo return failed
-		return
-	}
-	response = handler.Handle(request)
-	return
-}
-
-type LogHandler struct {
-	// ch chan Future
-}
-
-func (handler *LogHandler) Key() (key string) {
-
-	return
-}
-
-func (handler *LogHandler) Handle(request *AppendEntriesRequest) (response *AppendEntriesResponse) {
-
 	return
 }
